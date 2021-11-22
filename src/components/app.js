fe540d610e793696ddd10aa26b60923cb9e1b98e
Denis <rojkoff.d@gmail.com>
2021-11-22 20:12:31 +0300

    feature/ add todo

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,11 @@ import SearchPanel from "./search-panel";
 import TodoList from "./todo-list";
 import AppHeader from "./app-header";
 import ItemStatusFilter from "./item-status-filter";
+import ItemAddForm from "./item-add-form";
 import "./app.css";
 
 export default class App extends Component {
+  maxId = 100;
   state = {
     todoData: [
       { label: "Drink Coffee", important: false, id: 1 },
@@ -31,6 +33,23 @@ export default class App extends Component {
     });
   };
 
+  addItem = (text) => {
+    // generate id
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++,
+    };
+    // add element in array
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, newItem];
+      return {
+        todoData: newArr,
+      };
+    });
+
+    // console.log("Added", text);
+  };
   render() {
     return (
       <div className="todo-app">
@@ -39,8 +58,8 @@ export default class App extends Component {
           <SearchPanel />
           <ItemStatusFilter />
         </div>
-
         <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );
   }
