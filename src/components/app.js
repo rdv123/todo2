b9a2af3275544dc16650cbb2e429cf9abd91ac3c
Denis <rojkoff.d@gmail.com>
2021-11-24 21:44:56 +0300

    refactoring function onToggleDone/Important

--- src/components/app.js
+++ src/components/app.js
@@ -53,26 +53,28 @@ export default class App extends Component {
       };
     });
   };
+
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex((el) => el.id === id);
+    const oldItem = arr[idx];
+    const newItem = {
+      ...oldItem,
+      [propName]: !oldItem[propName],
+    };
+    return [...arr.slice(0, idx), newItem, ...arr.slice(idx + 1)];
+  }
   onToggleImportant = (id) => {
-    console.log("Toggle important", id);
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, "important"),
+      };
+    });
   };
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
       // 1. update objekt
-      const idx = todoData.findIndex((el) => el.id === id);
-      const oldItem = todoData[idx];
-      const newItem = {
-        ...oldItem,
-        done: !oldItem.done,
-      };
-      // 2. construct new array
-      const newArray = [
-        ...todoData.slice(0, idx),
-        newItem,
-        ...todoData.slice(idx + 1),
-      ];
       return {
-        todoData: newArray,
+        todoData: this.toggleProperty(todoData, id, "done"),
       };
     });
   };
---
+++ src/components/test.js
@@ -0,0 +1,21 @@
+let person = {
+  firstName: "Nic",
+  age: "44",
+  job: "m8",
+  adress: {
+    country: "Britan",
+    city: "London",
+  },
+};
+
+let [, , joneor] = ["pety", "vova", "denis"];
+// let pety =  arr[0]
+// let vova = arr[1]
+// let joneor = arr[2]
+console.log(joneor);
+
+let { firstName: gg } = person;
+console.log(gg);
+
+// let ggg = person.firstName
+// console.log(ggg)
