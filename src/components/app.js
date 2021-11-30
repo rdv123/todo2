64f6cb5db54f3aa7b077bb525b25ddb08f2ead36
Denis <rojkoff.d@gmail.com>
2021-11-30 19:28:48 +0300

    feature/ search

--- src/components/app.js
+++ src/components/app.js
@@ -15,6 +15,7 @@ export default class App extends Component {
       this.createTodoItem("Make awesome app"),
       this.createTodoItem("Have a lunch"),
     ],
+    term: "",
   };
 
   createTodoItem(label) {
@@ -90,9 +91,20 @@ export default class App extends Component {
   showAll = () => {
     console.log("all");
   };
-
+  onSearchChange = (term) => {
+    this.setState({ term });
+  };
+  search(items, term) {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+    });
+  }
   render() {
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
+    const visibleItems = this.search(todoData, term);
     const doneCount = todoData.filter((el) => el.done).length;
 
     const todoCount = todoData.length - doneCount;
@@ -101,7 +113,7 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel onSearchChange={this.onSearchChange} />
           <ItemStatusFilter
             onActive={this.showActive}
             onDone={this.showDone}
@@ -109,7 +121,7 @@ export default class App extends Component {
           />
         </div>
         <TodoList
-          todos={todoData}
+          todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
--- src/components/quokka1.js
+++ src/components/quokka1.js
@@ -2,6 +2,7 @@ var array = [5, 3, 2, 8, 1, 4];
 var indices = [];
 
 var array1 = array.filter((v, i) => v % 2 && indices.push(i));
+
 console.log(indices);
 console.log(array1);
 var array2 = array1.sort((a, b) => a - b);
